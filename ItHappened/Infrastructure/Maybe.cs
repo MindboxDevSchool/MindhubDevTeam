0f73f7f17d8b5ad7d270cfbe387d3e46041b5170
Arthur Khazbulatov <atkhazbulatov@edu.hse.ru>
2020-10-14 22:09:19 +0300

    Add Maybe<T> sum type

---
+++ ItHappened/Infrastructure/Maybe.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ItHappened.Infrastructure
+{
+    public abstract class Maybe<T>
+    {
+        private Maybe() { }
+
+        public sealed class Some : Maybe<T>
+        {
+            public Some(T value) => Value = value != null ? value : throw new ArgumentNullException(nameof(value));
+            public T Value { get; }
+        }
+
+        public sealed class None : Maybe<T> { }
+    }
+}
